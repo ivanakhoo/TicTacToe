7b61a24f152dbae4af965e1bde1da91d1a756357
Ivan Khoo <142810403+ivanakhoo@users.noreply.github.com>
2023-12-13 00:57:36 +0000

    Outline of the Game:

---
+++ TicTacToe.py
@@ -0,0 +1,44 @@
+# Tic Tac Toe - Ivan Khoo
+
+gameIsRunning = True
+currentPlayer = 'X'
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
+winner = None
+
+
+
+
+# Printing game board
+
+def printBoard(board):
+    print('-------')
+    print(board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('-------')
+    print(board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('-------')
+    print(board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print('-------')
+
+
+# Taking player input
+
+#def playerInput(board):
+
+# Switching Players
+
+#def switchPlayer(board)
+
+# Checking conditions
+
+#def checkRow(board):
+
+
+#def checkColumn(board):
+
+
+#def checkDiagonal(board):
+
+
+#def checkTie(board):
